3f11f95c8395f141b5ebc217bcd35ca292d0fe79
lukasz.derwisz11@gmail.com <lukasz.derwisz11@gmail.com>
2017-12-17 14:12:25 +0100

    1413update

---
+++ src/main/java/inz/ciscospark/CheckSize.java
@@ -0,0 +1,11 @@
+package inz.ciscospark;
+
+import java.util.List;
+
+public class CheckSize {
+	public int checkSize(List lista){
+		int ListaSize = lista.size();
+		return ListaSize;
+	}
+
+}
---
+++ src/main/java/inz/ciscospark/SoFBotMain.java
@@ -0,0 +1,17 @@
+package inz.ciscospark;
+
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+
+
+public class SoFBotMain {
+
+	public static void main(String[] args) throws URISyntaxException{
+	
+		GetInformation gI = new GetInformation();
+		gI.getInfrmation();
+		
+	}
+
+}
